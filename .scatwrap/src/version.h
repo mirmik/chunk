d18267df8acb8daf75eab315d61e7a6c626d7ec1
netricks <mirmikns@yandex.ru>
2025-11-29 02:52:49 +0300

    Update version to 0.2.0 and modify changelist for new commands

--- .scatwrap/src/version.h
+++ .scatwrap/src/version.h
@@ -11,11 +11,18 @@
 namespace&nbsp;chunk<br>
 {<br>
 inline&nbsp;constexpr&nbsp;int&nbsp;version_major&nbsp;=&nbsp;0;<br>
-inline&nbsp;constexpr&nbsp;int&nbsp;version_minor&nbsp;=&nbsp;1;<br>
+inline&nbsp;constexpr&nbsp;int&nbsp;version_minor&nbsp;=&nbsp;2;<br>
 inline&nbsp;constexpr&nbsp;int&nbsp;version_patch&nbsp;=&nbsp;0;<br>
 <br>
-inline&nbsp;constexpr&nbsp;const&nbsp;char&nbsp;*version_string&nbsp;=&nbsp;&quot;0.1.0&quot;;<br>
+inline&nbsp;constexpr&nbsp;const&nbsp;char&nbsp;*version_string&nbsp;=&nbsp;&quot;0.2.0&quot;;<br>
 }&nbsp;//&nbsp;namespace&nbsp;chunk<br>
+<br>
+/*&nbsp;CHANGELIST<br>
+<br>
+0.2.0&nbsp;Added&nbsp;append&nbsp;and&nbsp;prepend&nbsp;commands,&nbsp;object&nbsp;refactoring&nbsp;has&nbsp;been&nbsp;performed<br>
+0.1.0&nbsp;Initial<br>
+<br>
+*/<br>
 <!-- END SCAT CODE -->
 </body>
 </html>
--- src/version.h
+++ src/version.h
@@ -3,8 +3,15 @@
 namespace chunk
 {
 inline constexpr int version_major = 0;
-inline constexpr int version_minor = 1;
+inline constexpr int version_minor = 2;
 inline constexpr int version_patch = 0;
 
-inline constexpr const char *version_string = "0.1.0";
+inline constexpr const char *version_string = "0.2.0";
 } // namespace chunk
+
+/* CHANGELIST
+
+0.2.0 Added append and prepend commands, object refactoring has been performed
+0.1.0 Initial
+
+*/
