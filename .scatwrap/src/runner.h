d8e0f7f1e547379283e63643337c84a0ec62517e
netricks <mirmikns@yandex.ru>
2025-11-29 19:18:04 +0300

    'remove tests for apple'

--- .scatwrap/src/runner.h
+++ .scatwrap/src/runner.h
@@ -8,30 +8,12 @@
 <!-- BEGIN SCAT CODE -->
 #pragma&nbsp;once<br>
 <br>
-#pragma&nbsp;once<br>
-<br>
 #include&nbsp;&quot;command.h&quot;<br>
 <br>
-#include&nbsp;&lt;cstddef&gt;<br>
 #include&nbsp;&lt;memory&gt;<br>
-#include&nbsp;&lt;string&gt;<br>
 #include&nbsp;&lt;vector&gt;<br>
 <br>
-struct&nbsp;DryRunStats<br>
-{<br>
-&nbsp;&nbsp;&nbsp;&nbsp;struct&nbsp;CommandInfo<br>
-&nbsp;&nbsp;&nbsp;&nbsp;{<br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;const&nbsp;Command&nbsp;*cmd&nbsp;=&nbsp;nullptr;<br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;std::string&nbsp;filepath;<br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;std::size_t&nbsp;inserted&nbsp;=&nbsp;0;<br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;std::size_t&nbsp;erased&nbsp;=&nbsp;0;<br>
-&nbsp;&nbsp;&nbsp;&nbsp;};<br>
-<br>
-&nbsp;&nbsp;&nbsp;&nbsp;std::vector&lt;CommandInfo&gt;&nbsp;commands;<br>
-};<br>
-<br>
-DryRunStats&nbsp;apply_sections(const&nbsp;std::vector&lt;std::unique_ptr&lt;Command&gt;&gt;&nbsp;&amp;commands,<br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bool&nbsp;dry_run);<br>
+void&nbsp;apply_sections(const&nbsp;std::vector&lt;std::unique_ptr&lt;Command&gt;&gt;&nbsp;&amp;commands);<br>
 <!-- END SCAT CODE -->
 </body>
 </html>
