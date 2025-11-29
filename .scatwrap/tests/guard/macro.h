aacaa474000f354cffe25348848434d58490a330
netricks <mirmikns@yandex.ru>
2025-11-29 21:49:58 +0300

    'a'

--- .scatwrap/tests/guard/macro.h
+++ .scatwrap/tests/guard/macro.h
@@ -9,7 +9,7 @@
 #pragma&nbsp;once<br>
 <br>
 #define&nbsp;GUARD_STRINGIFY(...)&nbsp;#__VA_ARGS__<br>
-#define&nbsp;GUARD_STRINGIFY2(...)&nbsp;STRINGIFY(__VA_ARGS__)<br>
+#define&nbsp;GUARD_STRINGIFY2(...)&nbsp;GUARD_STRINGIFY_IMPL(__VA_ARGS__)<br>
 <br>
 #define&nbsp;GUARD_CONCAT(a,&nbsp;b)&nbsp;a##b<br>
 #define&nbsp;GUARD_CONCAT2(a,&nbsp;b)&nbsp;GUARD_CONCAT(a,&nbsp;b)<br>
